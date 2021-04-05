04b0e0a798de240879a39e8ea3c85d48bd2ab08f
oIITiMicT <backawork@gmail.com>
2021-04-05 22:04:48 +0200

    refactor code, add some comments

--- src/generator.c
+++ src/generator.c
@@ -1,15 +1,15 @@
 #include "generator.h"
 
-void generate(map* cur) {
-    int x = 10 + rand() % 50;  //от 10 + 49
+void generate(map *cur) {
+    int x = 10 + rand() % 50;  // field of size [10; 49]
     cur->n = x;
-    x = 10 + rand() % 50;
+    x = 10 + rand() % 50; // field of size [10; 49]
     cur->m = x;
     cur->arr = get_memory(cur->arr, cur->n, cur->m);
-    for (int i = 0 ; i < cur->n; i++) {
+    for (int i = 0; i < cur->n; i++) {
         for (int j = 0; j < cur->m; j++) {
             x = rand();
-            x %= 4; //шанс дропа 1(живой мобилы) = 100% / div(в данном случае 4) = 25% для изменения шанса меняйте 4 на любое другое число
+            x %= 4; // the chance of a living cell appearing - 25%
             cur->arr[i][j].live = x == 0 ? 1 : 0;
         }
     }
